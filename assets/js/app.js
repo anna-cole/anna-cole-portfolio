c72125b4f73d6330e373b9122ffc3648ec6039dc
Anna Raptidou Cole <acrrj123@gmail.com>
2024-06-23 22:38:04 -0500

    connect local repo to remote repo

---
+++ assets/js/app.js
@@ -0,0 +1,41 @@
+// Nav hamburgerburger selections
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+// Scroll to top selection
+const scrollUp = document.querySelector("#scroll-up");
+
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+
+// Select social icons 
+const socicons = document.querySelectorAll(".socicon");
+
+// Hamburger menu function
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
+});
+
+// Close hamburger menu when a link is clicked
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
+
+// scroll to top functionality
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+// Add CSS animation to social icons
+for (const i of socicons) {
+  i.addEventListener("mouseover", () => {
+    i.classList.add('animate__animated', 'animate__heartBeat');
+  })
+};
